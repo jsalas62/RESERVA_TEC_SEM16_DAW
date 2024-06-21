fb2ad77b2420eb6dafde158b2b725612cdb5cb64
Jorge Salas <jsalopdev@gmail.com>
2024-06-21 00:13:44 -0500

    despliegue_fixeds

--- src/main/java/com/reservatec/backendreservatec/BackendReservaTecApplication.java
+++ src/main/java/com/reservatec/backendreservatec/BackendReservaTecApplication.java
@@ -2,7 +2,6 @@ package com.reservatec.backendreservatec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class BackendReservaTecApplication {
@@ -10,5 +9,4 @@ public class BackendReservaTecApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendReservaTecApplication.class, args);
     }
-
 }
